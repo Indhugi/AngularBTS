8427a515aa568d9e6bf1605334c205c7668a6043
unknown <ibmjavanh12@iiht.tech>
2021-04-22 18:45:01 +0530

    simultaneous search implemented

--- src/app/get-bug/get-bug.component.ts
+++ src/app/get-bug/get-bug.component.ts
@@ -12,8 +12,8 @@ export class GetBugComponent implements OnInit {
   bug: Bug = new Bug();
   constructor(private bugService: BugService) { }
   bugList: any;
-  searchElement: any;
-  responseList: Boolean;
+  bugArray: Bug[] = [];
+
   deleteBug(bugId){
     let ask=confirm("Do you want to delete bug this bug ?");
     if (!ask){
@@ -32,54 +32,123 @@ export class GetBugComponent implements OnInit {
       }
     )
   }
+  // getBug() {
+  //   let bugStatus = (<HTMLInputElement>document.getElementById('bugStatus')).value;
+  //   let bugTitle = (<HTMLInputElement>document.getElementById('bugTitle')).value;
+  //   let endpointURL = 'http://localhost:8080/bug/';
+
+  //   if (Object.values(STATUS).includes(bugStatus)) {
+
+  //     endpointURL = endpointURL + 'status/' + bugStatus;
+  //     this.bugService.getBugByName(bugTitle).subscribe(response => {
+  //       this.bugList = response;
+  //       if(response!=null){
+  //         console.log(response);
+  //         alert('Bug Listed!!');
+  //       }
+  //       else{
+  //         alert("Bug with "+ bugStatus +" not found");
+  //       }
+  //     },
+  //       error => {
+  //         console.log(error);
+  //         alert("Error Happened!!");
+
+  //       }
+  //     )
+  //   }
+  //   else {
+  //     endpointURL = endpointURL + 'title/' + bugTitle;
+  //     this.bugService.getBugByStatus(bugStatus).subscribe(response => {
+  //       this.bugList = response;
+  //       if(response!=null){
+  //         console.log(response);
+  //         alert('Bug Listed!');
+  //       }
+  //       else{
+  //         alert("Bug with "+ bugTitle +" not found");
+  //       }
+
+  //     },
+  //       error => {
+  //         console.log(error);
+  //         alert("Error Happened!!");
+
+  //       }
+  //     )
+  //   }
+
+
+
+  // }
   getBug() {
-    let bugStatus = (<HTMLInputElement>document.getElementById('bugStatus')).value;
+    let status = (<HTMLInputElement>document.getElementById('bugStatus')).value;
     let bugTitle = (<HTMLInputElement>document.getElementById('bugTitle')).value;
-    let endpointURL = 'http://localhost:8080/bug/';
-
-    if (Object.values(STATUS).includes(bugStatus)) {
-
-      endpointURL = endpointURL + 'status/' + bugStatus;
-      this.bugService.getBug(endpointURL).subscribe(response => {
+    if (bugTitle && !status) {
+      if (bugTitle.trim()) {
+        const promise = this.bugService.getBugByName(bugTitle);
+        promise.subscribe(response => {
+          this.bugList = response;
+          if (this.bugList) {
+            alert("Bug Listed!!")
+            this.bugArray = this.bugList;
+          }
+          else {
+            alert("Record not found");
+            this.bugArray = [];
+          }
+        },
+          error => {
+            alert('Error Happened..')
+          });
+      }
+      else {
+        alert("please provide bug name");
+        this.bugArray = [];
+      }
+    }
+    else if (status && !bugTitle) {
+      const promise = this.bugService.getBugByStatus(status);
+      promise.subscribe(response => {
         this.bugList = response;
-        if(response!=null){
-          console.log(response);
-          alert('Bug Listed!!');
+        if (this.bugList) {
+          this.bugArray = this.bugList;
         }
-        else{
-          alert("Bug with "+ bugStatus +" not found");
+        else {
+          alert("No Bug with Status : " + status + " found");
+          this.bugArray = [];
         }
       },
         error => {
-          console.log(error);
-          alert("Error Happened!!");
-
+          alert('error happened..')
+        })
+    }
+    else if (bugTitle && status) {
+      const promise = this.bugService.getBugByNameAndStatus(bugTitle, status);
+      promise.subscribe(response => {
+        this.bugList = response;
+        if (this.bugList) {
+          this.bugArray = this.bugList;
         }
-      )
+        else {
+          alert("No Bug with Name : " + bugTitle + " and Status : " + status + " found");
+          this.bugArray = [];
+        }
+      },
+        error => {
+          alert('error happened..')
+        })
     }
     else {
-      endpointURL = endpointURL + 'title/' + bugTitle;
-      this.bugService.getBug(endpointURL).subscribe(response => {
+      const observable = this.bugService.getBugs();
+      observable.subscribe(response => {
         this.bugList = response;
-        if(response!=null){
-          console.log(response);
-          alert('Bug Listed!');
-        }
-        else{
-          alert("Bug with "+ bugTitle +" not found");
+        if (this.bugList) {
+          this.bugArray = this.bugList;
         }
 
-      },
-        error => {
-          console.log(error);
-          alert("Error Happened!!");
-
-        }
-      )
+      }, error => alert("Error occurred"));
     }
-
-
-
   }
    getBugs() {
     this.bugService.getBugs().subscribe(response => {
